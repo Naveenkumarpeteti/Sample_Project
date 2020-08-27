b24e7b14810f19c644f0c1c0e3154fc90907d536
root <root@ip-10-1-0-240.ec2.internal>
2020-08-27 10:11:38 +0000

    First commit

---
+++ test.py
@@ -0,0 +1,24 @@
+import unittest
+import calc
+
+class TestCalc(unittest.TestCase):
+	
+	def test_add(self):
+		self.assertEqual(calc.add(3,5),8)
+		self.assertEqual(calc.add(-1,1),0)
+		self.assertEqual(calc.add(-1,-1),-2)
+
+	def test_substract(self):
+		self.assertEqual(calc.substract(10,5),5)
+		self.assertEqual(calc.substract(-1,-1),0)	
+
+	def test_multiply(self):
+		self.assertEqual(calc.multiply(5,6),30)
+		self.assertEqual(calc.multiply(10,5),50)
+
+	def test_divide(self):
+		self.assertEqual(calc.divide(10,5),2)
+		self.assertEqual(calc.divide(6,3),2)
+
+if __name__ == '__main__':
+	unittest.main()
