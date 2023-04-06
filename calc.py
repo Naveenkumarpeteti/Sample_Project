70c02bef0a384fa1f0ee9a6f4ca1119eeb072cc7
Naveen Kumar <kumar.naveen099@gmail.com>
2023-04-06 10:49:09 +0530

    push change

--- calc.py
+++ calc.py
@@ -1,5 +1,5 @@
 #############################################
-####### test ha   s_key    dds new    d ww s_key test  file  test fi  eno  gg   ss test
+####### test ha   s_key    dds new    d ww s_key test  file  test fi  eno  gg   ss test 1
 def add(x,y):
 	return x+y
 
