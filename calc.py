8639fe5fca279ecce7499fa84ccd0fd245300905
Naveen Kumar <kumar.naveen099@gmail.com>
2023-05-19 10:33:16 +0530

    commit1

--- calc.py
+++ calc.py
@@ -12,7 +12,7 @@ def multiply(x,y): test
 	return x*y    s
 
 def divide(x,y):
-	if y == 0:   value 1  helloi  sample change.
+	if y == 0:   value 1  helloi  sample change.  test1
 		raise ValueError('Can not divide by zero!')
 	return x/y
 
